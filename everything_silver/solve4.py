fde0757751d0dd4075d706a070911ec8a820d041
Xornet <einnotwendigesubel@gmail.com>
2024-11-18 19:11:27 +0900

    using hooks

--- everything_silver/solve4.py
+++ everything_silver/solve4.py
@@ -39,6 +39,10 @@ xs = [
     0x83124ea1,
     0x4da78b03,
 ]
+
+base_addr = 0
+current_addr = len(shellcode)
+hook_addrs = []
 for x in xs:
     # mov    eax, DWORD PTR [rdi]
     shellcode += b"\x8b\x07"
@@ -48,9 +52,9 @@ for x in xs:
     shellcode += b
     eax_mul += 0xc0ffee
 
-    # add new operation
-    # mov dword ptr [rdi], eax
-    shellcode += b"\x89\x07"
+    # imul eax, eax, ??? を実行後にフックするためのアドレスを取得
+    current_addr = len(shellcode)
+    hook_addrs.append(base_addr + current_addr)
 
     # add    rdi, 0x4
     shellcode += b"\x48\x83\xc7\x04"
@@ -59,13 +63,25 @@ for x in xs:
 for op in md.disasm(shellcode, 0):
     print(f"0x{op.address:x}: {op.mnemonic} {op.op_str}")
 
+
+def add_constraint(x):
+    def solver_hook(st):
+        eax = st.regs.eax
+        st.solver.add(eax == x)
+
+    return solver_hook
+
 # ==========================================================================
 # https://docs.angr.io/en/stable/api.html#angr.load_shellcode
 proj = angr.load_shellcode(shellcode, arch="amd64")
 
+for addr, x in zip(hook_addrs, xs):
+    proj.hook(addr, add_constraint(x))
+
+
 # 初期状態の作成 with おまじない
 state = proj.factory.blank_state(
-    addr=0x0,
+    addr=base_addr,
     add_options={
         "ZERO_FILL_UNCONSTRAINED_MEMORY",
         "ZERO_FILL_UNCONSTRAINED_REGISTERS"
@@ -91,16 +107,11 @@ simgr.explore(find=target_addr)
 if simgr.found:
     found_state = simgr.found[0]
     regs = found_state.regs
-    mem = found_state.mem
-
-    for i, x in enumerate(xs):
-        # print(mem[buf_addr + 4*i].uint32_t.resolved == x)
-        found_state.solver.add(mem[buf_addr + 4*i].uint32_t.resolved == x)
 
     # 多分要らん
-    for i in range(input_size):
-        b = sym_input.get_byte(i)
-        found_state.solver.add(b < 0x7f)
+    # for i in range(input_size):
+    #     b = sym_input.get_byte(i)
+    #     found_state.solver.add(b < 0x7f)
     solution = found_state.solver.eval(sym_input, cast_to=bytes)
     print("Found solution:", solution)
 else:
